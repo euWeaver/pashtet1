9453b2c68927cc20d872f05f3c292f252d7fbbdb
euWeaver <102436061+euWeaver@users.noreply.github.com>
2023-10-30 03:39:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga4";
 
+const trackingId = "G-YSFDG6Q19W"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 ReactDOM.render(
   <Router>
     <App />
