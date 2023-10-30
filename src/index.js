a9c8043d228c788bb3aa42e47e786d30a9a24d7a
euWeaver <102436061+euWeaver@users.noreply.github.com>
2023-10-30 04:01:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import ReactGA from "react-ga4";
 
 const trackingId = "G-YSFDG6Q19W"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
+
 ReactDOM.render(
   <Router>
     <App />
