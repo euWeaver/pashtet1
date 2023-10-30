60f8fd57c186c5827d0aa2cb63a5f477d1e4a278
euWeaver <102436061+euWeaver@users.noreply.github.com>
2023-10-30 03:39:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import SecondPage from "./components/SecondPage";
 import ThirdPage from "./components/ThirdPage";
 import FourthPage from "./components/FourthPage";
 import EmailContext from "./components/EmailContext";
-import ReactGA from "react-ga";
 function App() {
   const [email, setEmail] = useState("");
-  const trackingId = "G-YSFDG6Q19W"; // Replace with your Google Analytics tracking ID
-  ReactGA.initialize(trackingId);
+
   return (
     <div className="App">
       <Switch>
